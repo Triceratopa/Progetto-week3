e1183e03a3309e7c5482a44cd9aef0c866d993ea
Giulia Gallo <giulia.gallo2903@gmail.com>
2024-10-19 21:37:54 +0200

    s

---
+++ ripasso.js
@@ -0,0 +1,43 @@
+//esercizio n 1
+let name= 'giulia'
+console.log(name)
+let age='27'
+console.log(age)
+//esercizio 2
+let n1=10
+let n2=35
+console.log('somma',n1+n2)
+//esercizio3 
+let saluto='ciao'
+let persona='giulia'
+console.log(saluto, persona, ' come stai?')
+//esercizio 4
+let n3= 8
+if(n3%2===0){
+  console.log('pari')
+}else{
+    console.log('dispari')
+}
+ // esercizio 1
+ const fruit=['apple','strawberry','pear','watermelon','banana']
+ fruit.push('kiwi')
+ fruit.shift()
+ console.log(fruit)
+ //esercizio 2
+ const car={
+    marca:'Tesla',
+    modello: 'y',
+    anno: '2016',
+ }
+ car.colore='black'
+ console.log(car)
+ //eserizio 3
+ const book = [{
+    title: 'harry potter 1',
+    autore:'j.k.rowling',},
+ 
+
+   { title: 'il signore degli anelli',
+    autore: 'tolkien'
+}]
+console.log(book)
